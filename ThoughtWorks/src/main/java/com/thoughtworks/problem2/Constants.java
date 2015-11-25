c62918a46e462ed022e11c454725c83441b917c1
achuth <achuth.i@gmail.com>
2015-11-26 02:13:50 +0800

    Updated Variable Name

--- ThoughtWorks/src/main/java/com/thoughtworks/problem2/Constants.java
+++ ThoughtWorks/src/main/java/com/thoughtworks/problem2/Constants.java
@@ -3,7 +3,7 @@ package com.thoughtworks.problem2;
 import java.math.BigDecimal;
 
 public class Constants {
-	public static final BigDecimal ZERO_POINT_ZERO_ONE = new BigDecimal(".1");
+	public static final BigDecimal ZERO_POINT_ONE = new BigDecimal(".1");
 	public static final BigDecimal ZERO_POINT_ZERO_FIVE = new BigDecimal(".05");
 	public static final BigDecimal HUNDRED = new BigDecimal("100");
 	public static final BigDecimal FIVE = new BigDecimal("5");
--- ThoughtWorks/src/main/java/com/thoughtworks/problem2/items/BasicSalesTaxGood.java
+++ ThoughtWorks/src/main/java/com/thoughtworks/problem2/items/BasicSalesTaxGood.java
@@ -1,11 +1,11 @@
 package com.thoughtworks.problem2.items;
 
 import java.math.BigDecimal;
-import static com.thoughtworks.problem2.Constants.ZERO_POINT_ZERO_ONE;
+import static com.thoughtworks.problem2.Constants.ZERO_POINT_ONE;
 
 public abstract class BasicSalesTaxGood extends Good {
 	@Override
 	BigDecimal getSalesTaxAmount() {
-		return getPrice().multiply(ZERO_POINT_ZERO_ONE);
+		return getPrice().multiply(ZERO_POINT_ONE);
 	}
 }
