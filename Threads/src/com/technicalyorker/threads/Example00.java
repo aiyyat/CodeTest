b809debff7260ca1b6bc6e7c6f3810440cecdbc8
achuth <achuth.i@gmail.com>
2016-02-18 08:34:38 +0800

    Added comments

--- Threads/src/com/technicalyorker/threads/Example00.java
+++ Threads/src/com/technicalyorker/threads/Example00.java
@@ -1,32 +1,42 @@
 package com.technicalyorker.threads;
 
-public class Example00 {
+/**
+ * Need for Volatility. Some JVM tries to Optimize variables by caching it when
+ * it comes to thread as if no other thread would be expected to be modifying
+ * the variable. These thread cached variables are local copies to threads and
+ * hence a change from outside the thread is not visible to the thread itself.
+ * The following program without volatile could potentially go into an infinite
+ * loop with some JVMs.
+ * 
+ * @author achuth
+ *
+ */
+public class Example00 extends Thread {
 	volatile boolean stop = false;
 
 	public static void main(String[] args) {
 		Example00 e = new Example00();
-		e.perform();
+		e.start();
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
-		e.stop = true;
+		e.stopNow();
 	}
 
-	private void perform() {
-		new Thread() {
-			@Override
-			public void run() {
-				while (!stop) {
-					System.out.println("Running");
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
+	private void stopNow() {
+		stop = true;
+	}
+
+	public void run() {
+		while (!stop) {
+			System.out.println("Running");
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		}.start();
+		}
 	}
 }
