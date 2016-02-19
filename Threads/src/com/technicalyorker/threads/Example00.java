7e2223b7e7d41e8cabc9a29c6eee6eed8b7efab3
achuth <achuth.i@gmail.com>
2016-02-20 07:17:25 +0800

    NFV

--- Threads/src/com/technicalyorker/threads/Example00.java
+++ Threads/src/com/technicalyorker/threads/Example00.java
@@ -1,12 +1,13 @@
 package com.technicalyorker.threads;
 
 /**
- * Need for Volatility. Some JVM tries to Optimize variables by caching it when
- * it comes to thread as if no other thread would be expected to be modifying
- * the variable. These thread cached variables are local copies to threads and
- * hence a change from outside the thread is not visible to the thread itself.
- * The following program without volatile could potentially go into an infinite
- * loop with some JVMs.
+ * Need for Volatility.
+ * 
+ * Some JVMs try to Optimize variables by caching it when it comes to thread as
+ * if no other thread would be expected to be modifying the variable. These
+ * thread cached variables are local copies to threads and hence a change from
+ * outside the thread is not visible to the thread itself. The following program
+ * without volatile could potentially go into an infinite loop with some JVMs.
  * 
  * @author achuth
  *
