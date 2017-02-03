63eff7edd913e2e338639309d9b8dcbb7503b4f1
technicalyorker <achuth.i@gmail.com>
2017-02-03 06:50:50 +0530

    Help screenshot updated

--- Infosys/Calculator/src/main/java/com/technicalyorker/calculator/Calculator.java
+++ Infosys/Calculator/src/main/java/com/technicalyorker/calculator/Calculator.java
@@ -11,8 +11,8 @@ public class Calculator {
 	public static void main(String[] args) {
 		if (args.length < 1 || (args.length >= 1 && "--help".equals(args[0]))) {
 			System.out.println(
-					"Welcome to Technical Yorker Calculator App!!\n Operations Supported in this release: \n\t\t+\n\t\t-\n\t\t/\n\t\t*\n\t\t^\n\t\tSine\n\t\tTan\n\n Usage: Calculator <expression to Calculate> \n"
-							+ " e.g. Calculator \"3 * ( 4 + 5 )\" \n Calculator 3 * Sin 30");
+					"Welcome to Technical Yorker Calculator App!!\n Operations Supported in this release: \n\t\t+\n\t\t-\n\t\t/\n\t\t*\n\t\t^\n\t\tSine\n\t\tTan\n\n Usage: java -jar Calculator-<version>.jar \"<expression to Calculate>\" \n"
+							+ " e.g. java -jar Calculator-1.0-SNAPSHOT.jar \"3 * ( 4 + 5 )\" \n Calculator-1.0-SNAPSHOT.jar \"3 * Sine 30\"");
 		} else {
 			System.out.printf("Input: %s \nOutput: %s\n", args[0], engine.calculate(args[0]));
 		}
